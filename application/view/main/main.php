899e325bf9e75f1688e6e6b99d8ec3312f7bbdd3
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2016-05-11 12:46:07 +0200

    hotfix #1

--- application/view/main/main.php
+++
@@ -1,7 +0,0 @@
-<nav>
-	<a href="<?php echo URL; ?>leaderboard/index.php" ><button>Leaderboard</button></a>
-	<a href="<?php echo URL; ?>main/main.php" ><button>Home</button></a><br>
-	<a href="<?php echo URL; ?>main/information.php" ><button>Information</button></a>
-	<a href="https://www.google.com" ><button>Quit</button></a>
-</nav>
-<a href="<?php echo URL; ?>quistions/quistionIntro" ><button id="specialButton" >Start Quiz</button></a>
