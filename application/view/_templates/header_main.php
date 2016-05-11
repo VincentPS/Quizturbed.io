9bebd5bb677513af4fec4c9dc15f35e79da7ba66
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2016-05-11 17:57:55 +0200

    hotfix 8
    just added and fixed some stuf

--- application/view/_templates/header_main.php
+++ application/view/_templates/header_main.php
@@ -4,9 +4,10 @@
 		<title>Quizturbed.io</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/style.css">
+		<link href='https://fonts.googleapis.com/css?family=Raleway:900' rel='stylesheet' type='text/css'>
 		<script src="<?php echo URL; ?>js/rainbows.js" type="text/javascript"></script>
 	</head>
 	<body id="body">
 		<main id="main">
-			<h1>Q</h1><h2>uizturbed</h2>
 			<section id="section">
+				<h1><span class="Q" >Q</span>uizturbed</h1><br>
