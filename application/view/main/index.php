3d033ef8322d9bca1899700c76ed4d1069aed85d
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2016-05-11 13:11:22 +0200

    hotfix #2
    fixed some stuf

--- application/view/main/index.php
+++ application/view/main/index.php
@@ -1,6 +1,6 @@
 <nav>
 	<a href="<?php echo URL; ?>" ><button>Home</button></a>
-	<a href="<?php echo URL; ?>leaderboard/index" ><button>Leaderboard</button></a>
+	<a href="<?php echo URL; ?>main/leaderboard" ><button>Leaderboard</button></a>
 	<a href="<?php echo URL; ?>main/information" ><button>Information</button></a>
 	<a href="https://www.google.com" ><button>Quit</button></a>
 </nav>
--- application/view/main/leaderboard.php
+++ application/view/main/leaderboard.php
@@ -4,4 +4,6 @@
 	<a href="<?php echo URL; ?>main/information" ><button>Information</button></a>
 	<a href="https://www.google.com" ><button>Quit</button></a>
 </nav>
-<button id="specialButton" >An error accured!<br>You might have entered the wrong url or your trying to break the website but you did something wrong xD</button>
+<?php
+readScore('user', 'score');
+?>
