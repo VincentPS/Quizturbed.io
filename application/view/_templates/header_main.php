402975ec9f1acdd62c6293d0baf8eb421bb4501c
VincentPS <vincentschaddelee@hotmail.com>
2016-05-12 11:28:21 +0200

    Revert "hotfix #21"

--- application/view/_templates/header_main.php
+++ application/view/_templates/header_main.php
@@ -4,20 +4,9 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/style.css">
 		<link href='https://fonts.googleapis.com/css?family=Raleway:200,900' rel='stylesheet' type='text/css'>
-		<script src="<?php echo URL; ?>js/title.js" type="text/javascript"></script>
+		<script src="<?php echo URL; ?>js/rainbows.js" type="text/javascript"></script>
 	</head>
 	<body id="body">
 			<header>
-				<h1>
-					<span id="Q" class="Q" >Q</span>
-					<span id="u" >u</span>
-					<span id="i" >i</span>
-					<span id="z" >z</span>
-					<span id="t" >t</span>
-					<span id="u" >u</span>
-					<span id="r" >r</span>
-					<span id="b" >b</span>
-					<span id="e" >e</span>
-					<span id="d" >d</span>
-				</h1><br>
+				<h1 class="glitch"><span class="Q glitch" >Q</span>uizturbed</h1><br>
 				<p class="sub-title">The Disturbing Quiz</p>
