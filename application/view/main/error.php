7c5cc158fed9a7a5610da4cdd65f08f69f803832
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2016-05-12 09:22:10 +0200

    Hotfix #20

--- application/view/main/error.php
+++ application/view/main/error.php
@@ -1,8 +1,8 @@
 	<nav>
-		<a href="<?php echo URL; ?>" ><button class="marginFix" >Home</button></a>
-		<a href="<?php echo URL; ?>main/leaderboard" ><button class="marginFix" >Leaderboard</button></a>
-		<a href="<?php echo URL; ?>main/information" ><button class="marginFix" >Info</button></a>
-		<a href="https://www.google.com" ><button>Quit</button></a>
+		<a href="<?php echo URL; ?>" ><button class="marginFix buttonInverted" >Home</button></a>
+		<a href="<?php echo URL; ?>main/leaderboard" ><button class="marginFix buttonInverted" >Leaderboard</button></a>
+		<a href="<?php echo URL; ?>main/information" ><button class="marginFix buttonInverted" >Info</button></a>
+		<a href="https://www.google.com" ><button class="buttonInverted" >Quit</button></a>
 	</nav>
 </header>
 <main>
